8a1899284eedc3f4172923dfc672466e6541a0c8
Gayatri Lamba <lambagayatri19@gmail.com>
2023-12-30 22:22:45 +0530

    latest  commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SearchItem from './SearchItem';
 import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL=  'http://localhost:3500/items';
+  const API_URL= 'https://fakebackendtodo-b22208321fa0.herokuapp.com/items';
 
   const [items, setItems]=useState([]); 
   const [newItem, setNewItem]=useState(' ')
